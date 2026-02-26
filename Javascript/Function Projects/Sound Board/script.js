d2e91774ceed2fb67e90bb3e5128d812d9ffe0c3
Abinaya Goud <168286063+abinayagoudjandhyala@users.noreply.github.com>
2026-02-26 20:14:52 +0530

    Function projects

---
+++ Javascript/Function Projects/Sound Board/script.js
@@ -0,0 +1,12 @@
+document.getElementById('sound1').addEventListener('click', function() {
+    const audio = new Audio('sound1.mp3');
+    audio.play();
+});
+document.getElementById('sound2').addEventListener('click', function() {
+    const audio = new Audio('sound2.mp3');
+    audio.play();
+});
+document.getElementById('sound3').addEventListener('click', function() {
+    const audio = new Audio('sound3.mp3');
+    audio.play();
+});
